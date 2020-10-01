33db8bbf67458f686b2a71ed93d5c3b95f229175
ThibautFaitDesTartiflettes <71119942+ThibautFaitDesTartiflettes@users.noreply.github.com>
2020-10-01 22:48:42 +0200

    page accueil v1.3 'final'

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <header>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
-            <a class="navbar-brand" href="#">LOGO</a>
+            <a class="navbar-brand" href="#"><img src="includes/img/campfire2.gif" width="60" height="60"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -21,13 +21,28 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-lg-auto">
                 <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
+                <a class="nav-link" href="#">Profil</a>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link" href="#">Portfolio</a>
+                <a class="nav-link" href="#">PPE</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Stage</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Développement</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">CyberEdu</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Veilles</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">CV</a>
                 </li>
                 <li class="nav-item">
                 <a class="nav-link" href="#">Contact</a>
@@ -40,15 +55,22 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum ad vero sapiente, tempora enim nam at dolorem assumenda deserunt, odit eum optio, alias vitae beatae. Vero ratione accusantium perferendis iure.</p>
-                <a href="#" class="btnD1">Read More</a>
+                <h2 id="typed"></h2>
+                <p>Une tasse de café ? Un petit gâteau ? Alors vous êtes prêt pour la visite, choisissez donc une option dans le menu ci-dessus, sur le bouton en dessous ou scrollez ! <br><strong>Bonne visite !</strong></p>
+                <a href="#" class="btnD1">M'emmener quelque part</a>
             </div>
         </div>
     </div>
 </section>
+<section>
+    <p>Si debugger, c’est supprimer des bugs, alors programmer ne peut être que les ajouter — Edsger Dijkstra</p>
+</section>
 
     <!-- Optional JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+    <script>
+		var typed = new Typed('#typed', { strings: ["Mon incroyable <br> portfolio !"], typeSpeed: 75, cursorChar: '_', showCursor: false });
+	</script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
